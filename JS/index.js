5879da83f474712cb4bda4ff6fc3bc4fe2b78995
Ser1zard <stefano.cozzolino74@gmail.com>
2026-01-28 01:06:34 +0100

    Fourth Commit

--- JS/index.js
+++ JS/index.js
@@ -1,17 +1,21 @@
+const explosionSound = new Audio("AUDIO/explosion.mp3");
+explosionSound.volume = 0.3;
+
 document.getElementById('email_button_btn').addEventListener('click', function() {
-    // Hide the email notification and button
     document.getElementById('email_notification').style.display = 'none';
     document.getElementById('email_button').style.display = 'none';
 
-    // Show the opened email image
     document.getElementById('explosion').style.display = 'block';
+    document.getElementById('invitation').style.display = 'block';
+
+    explosionSound.currentTime = 0;
+    explosionSound.play().then(r => 0);
+
+    setTimeout(function() {
+        document.getElementById('invitation').style.opacity = '1';
+    }, 10);
 
     setTimeout(function() {
         document.getElementById('explosion').style.display = 'none';
-        document.getElementById('invitation').style.display = 'block';
-
-        setTimeout(function() {
-            document.getElementById('invitation').style.opacity = '1';
-        }, 10);
     }, 750);
 });
